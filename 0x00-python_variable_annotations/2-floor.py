67e3c44a14788d68426fafcb1e978498b517136a
juanfe9118 <1168@holbertonschool.com>
2020-10-19 14:52:59 -0500

    Add floor

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+'''
+Module that takes a float and returns the floor of that float
+'''
+
+
+def floor(n: float) -> int:
+    '''
+    Type-annotated function that takes a float and returns the floor as int
+
+    @n: float that will be floored
+    Return: floor of n as int
+    '''
+    return int(n)
