5752e761d5be8e7d21eded99703dba94648c0178
juanfe9118 <1168@holbertonschool.com>
2020-10-20 14:26:27 -0500

    Add basic async

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 '''
-Module to 
+Module that takes an integer and waits for a random delay between 0 and the
+integer and returns the delay time in float
 '''
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Function that takes an integer and waits for a random delay between 0 and
+    the integer
+
+    @max_delay: the integer taken in as parameter
+    Return: the waited time in float
+    """
+    rand = random.uniform(0, max_delay)
+    await asyncio.sleep(rand)
+    return rand
