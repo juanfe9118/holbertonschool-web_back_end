448309d2b2f83d14df890d627d3c02fa0d6bda76
juanfe9118 <1168@holbertonschool.com>
2020-10-20 18:55:27 -0500

    Add second task file

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 '''
-Module to 
+Module that does the same as wait_n but using task_wait_random instead
 '''
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    Function that calls wait_random and spawns it n times asynchronously
+
+    @n: the amount of times wait_random will be called
+    @max_delay: the max delay passed to wait_random
+    Return: the list of waited times in float
+    '''
+    tasks = [asyncio.gather(task_wait_random(max_delay)) for _ in range(n)]
+    time_list = []
+    for task in asyncio.as_completed(tasks):
+        time: float = await task
+        time_list.append(time)
+    return time_list
