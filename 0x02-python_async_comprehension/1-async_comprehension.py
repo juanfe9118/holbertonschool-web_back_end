b247fbdd361b04852f077d9328af0987550da4ab
juanfe9118 <1168@holbertonschool.com>
2020-10-21 16:19:00 -0500

    Add async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,4 +1,15 @@
 #!/usr/bin/env python3
 '''
-Module that 
+Module that imports async generator and creates a comprehension of it
 '''
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    '''
+    Function that comprehenses over async_generator and returns the list of
+    floats
+    '''
+    return [num async for num in async_generator()]
