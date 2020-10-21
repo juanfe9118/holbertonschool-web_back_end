51c48cac5c378553f1f7b71d791b01b11194d3a1
juanfe9118 <1168@holbertonschool.com>
2020-10-21 16:05:43 -0500

    Add async generator and make files executable

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 '''
-Module that 
+Module that creates a coroutine that yields a random number
 '''
+import asyncio
+from random import uniform
+from typing import Generator
+
+
+async def async_generator() -> Generator[float, None, None]:
+    '''
+    Function that creates a generator that yields a random number between 0-10
+    '''
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield uniform(0, 10)
