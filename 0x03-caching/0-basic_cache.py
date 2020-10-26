b401376cabe0c4a59a4ce56698ea008503ab65b0
juanfe9118 <1168@holbertonschool.com>
2020-10-26 18:01:01 -0500

    Add basic cache

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -2,3 +2,27 @@
 '''
 Basic caching module
 '''
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    '''
+    Basic caching class that inherits from BaseCaching
+    '''
+
+    def put(self, key, item):
+        '''
+        Method that put a new key, value pair to the cache
+        '''
+        if (key and item):
+            self.cache_data[key] = item
+
+    def get(self, key):
+        '''
+        Method that retrieves the item associated with key
+        '''
+        if key:
+            for k in self.cache_data.keys():
+                if key == k:
+                    return self.cache_data.get(key)
+        return None
