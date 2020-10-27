1691b8cbae7059a5fdff0e1163201ea9dd6de8ea
juanfe9118 <1168@holbertonschool.com>
2020-10-26 19:27:44 -0500

    Add LFU cache

--- 0x03-caching/3-lru_cache.py
+++ 0x03-caching/3-lru_cache.py
@@ -1,4 +1,44 @@
 #!/usr/bin/python3
 '''
-Basic caching module
+FIFO caching module
 '''
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    '''
+    LRU caching class that inherits from BaseCaching and is a caching system
+    '''
+
+    def __init__(self):
+        '''
+        Initialize
+        '''
+        super().__init__()
+        self.keys = []
+
+    def put(self, key, item):
+        '''
+        Method that puts a new key-value pair to the cache
+        '''
+        if (key and item):
+            self.cache_data[key] = item
+            if key in self.keys:
+                self.keys.remove(key)
+                self.keys.append(key)
+            else:
+                self.keys.append(key)
+            if len(self.keys) > self.MAX_ITEMS:
+                erase = self.keys.pop(0)
+                del self.cache_data[erase]
+                print('DISCARD: {}'.format(erase))
+
+    def get(self, key):
+        '''
+        Method that retrieves the item associated with key
+        '''
+        if key and key in self.keys:
+            self.keys.remove(key)
+            self.keys.append(key)
+            return self.cache_data[key]
+        return None
