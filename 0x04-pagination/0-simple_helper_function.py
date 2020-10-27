aa0b358afa6b2b2bb63282c8d2a56cb22750514e
juanfe9118 <1168@holbertonschool.com>
2020-10-27 12:40:21 -0500

    Fix return value type

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -2,9 +2,10 @@
 '''
 Module for simple helper function to pagination
 '''
+from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> tuple:
+def index_range(page: int, page_size: int) -> Tuple(int, int):
     '''
     Simple helper function to pagination
 
