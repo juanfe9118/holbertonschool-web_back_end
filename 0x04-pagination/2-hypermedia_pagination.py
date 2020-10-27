b5491bcf82ef620033b24d55d79341fc7fd4b3c2
juanfe9118 <1168@holbertonschool.com>
2020-10-27 16:02:30 -0500

    Add hypermedia pagination

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -1,4 +1,81 @@
 #!/usr/bin/env python3
 '''
-Docstring
+Module that returns the list of results for a given page
 '''
+import csv
+import math
+from typing import List, Tuple, Dict
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    '''
+    Simple helper function to pagination
+
+    @page: the page number
+    @page_size: the amount of results per page
+    Return: a tuple of the starting and end indexes for that page
+    '''
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        '''
+        Gets the results per page of the given page
+        '''
+        assert type(page) == int and page > 0
+        assert type(page_size) == int and page_size > 0
+        page_list = []
+        results = self.dataset()
+        indexes = index_range(page, page_size)
+        try:
+            for i in range(indexes[0], indexes[1]):
+                page_list.append(results[i])
+        except IndexError:
+            pass
+        return page_list
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        '''
+        Gets the results per page of the given page and returns a dictionary
+        '''
+        hyper_dict = {}
+        page_list = self.get_page(page, page_size)
+        act_page_size = len(page_list)
+        total_pages = math.ceil(len(self.dataset()) / page_size)
+        if page < total_pages:
+            nextp = page + 1
+        else:
+            nextp = None
+
+        if page != 1:
+            prev = page - 1
+        else:
+            prev = None
+        hyper_dict["page_size"] = act_page_size
+        hyper_dict["page"] = page
+        hyper_dict["data"] = page_list
+        hyper_dict["next_page"] = nextp
+        hyper_dict["prev_page"] = prev
+        hyper_dict["total_pages"] = total_pages
+        return hyper_dict
