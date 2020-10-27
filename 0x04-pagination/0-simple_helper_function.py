d354627939e8df959d55f4abefd64efe5ceccec3
juanfe9118 <1168@holbertonschool.com>
2020-10-27 12:13:45 -0500

    Make files executable and add helper function

--- 0x04-pagination/0-simple_helper_function.py
+++ 0x04-pagination/0-simple_helper_function.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 '''
-Docstring
+Module for simple helper function to pagination
 '''
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    '''
+    Simple helper function to pagination
+
+    @page: the page number
+    @page_size: the amount of results per page
+    Return: a tuple of the starting and end indexes for that page
+    '''
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
